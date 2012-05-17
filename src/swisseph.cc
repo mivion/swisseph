875234ffa7b87bfe794e2b5b06b847de6333f63f
mivion <onopin.m.v@gmail.com>
2012-05-17 16:45:27 +0400

    Added swe_julday function

--- src/swisseph.cc
+++ src/swisseph.cc
@@ -1,15 +1,44 @@
 #include <node.h>
 #include <v8.h>
 
+#include "swephexp.h"
+
 using namespace v8;
 
-Handle<Value> Hello (const Arguments & args) {
-  HandleScope scope;
-  return scope.Close (String::New ("world"));
+/**
+ * double swe_julday(int year, int month, int day, double hour, int gregflag)
+ */
+Handle<Value> node_swe_julday (const Arguments & args) {
+	HandleScope scope;
+
+	if (args.Length () < 5) {
+		ThrowException (Exception::TypeError (String::New ("Wrong number of arguments")));
+		return scope.Close (Undefined ());
+	};
+
+	if (
+		!args [0]->IsNumber () ||
+		!args [1]->IsNumber () ||
+		!args [2]->IsNumber () ||
+		!args [3]->IsNumber ()
+	) {
+		ThrowException (Exception::TypeError (String::New ("Wrong type of arguments")));
+		return scope.Close (Undefined ());
+	};
+
+	Local <Number> result = Number::New (::swe_julday (
+		(int)args [0]->NumberValue (),
+		(int)args [1]->NumberValue (),
+		(int)args [2]->NumberValue (),
+		(int)args [3]->NumberValue (),
+		(int)args [4]->NumberValue ()
+	));
+
+	return scope.Close (result);
 }
 
 void Initialize (Handle <Object> target) {
-  NODE_SET_METHOD (target, "hello", Hello);
+	NODE_SET_METHOD (target, "swe_julday", node_swe_julday);
 }
 
 NODE_MODULE (swisseph, Initialize);
--- test/test.js
+++ test/test.js
@@ -1,6 +1,18 @@
 var assert = require ('assert');
 var swisseph = require ('..');
 
-assert.equal ('world', swisseph.hello ());
 
-console.log ('swisseph.hello () =', swisseph.hello ());
+{ // Julian day
+	var date = {
+		year: 2012,
+		month: 1,
+		day: 1,
+		hour: 0,
+	};
+
+	var julday = swisseph.swe_julday (date.year, date.month, date.day, date.hour, swisseph.SE_GREG_CAL);
+
+	assert.equal (julday, 2455927.5);
+
+	console.log ('swisseph.swe_julday (' + date.year + ', ' + date.month + ', ' + date.day + ', ' + date.hour + ', SE_GREG_CAL) =', julday);
+}
