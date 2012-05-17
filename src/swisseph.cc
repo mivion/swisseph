2ba05ed1efa26a8d206c39caa08563e56fb6fddc
mivion <onopin.m.v@gmail.com>
2012-05-17 17:28:01 +0400

    Added swe_calc_ut

--- src/swisseph.cc
+++ src/swisseph.cc
@@ -7,6 +7,8 @@ using namespace v8;
 
 /**
  * double swe_julday(int year, int month, int day, double hour, int gregflag)
+ * =>
+ * double swe_julday(int year, int month, int day, double hour, int gregflag)
  */
 Handle<Value> node_swe_julday (const Arguments & args) {
 	HandleScope scope;
@@ -20,7 +22,8 @@ Handle<Value> node_swe_julday (const Arguments & args) {
 		!args [0]->IsNumber () ||
 		!args [1]->IsNumber () ||
 		!args [2]->IsNumber () ||
-		!args [3]->IsNumber ()
+		!args [3]->IsNumber () ||
+		!args [4]->IsNumber ()
 	) {
 		ThrowException (Exception::TypeError (String::New ("Wrong type of arguments")));
 		return scope.Close (Undefined ());
@@ -30,15 +33,76 @@ Handle<Value> node_swe_julday (const Arguments & args) {
 		(int)args [0]->NumberValue (),
 		(int)args [1]->NumberValue (),
 		(int)args [2]->NumberValue (),
-		(int)args [3]->NumberValue (),
+		args [3]->NumberValue (),
 		(int)args [4]->NumberValue ()
 	));
 
 	return scope.Close (result);
 }
 
+/**
+ * int32 swe_calc_ut(double tjd_ut, int32 ipl, int32 iflag, double *xx, char *serr)
+ * =>
+ * swe_calc_ut(double tjd_ut, int32 ipl, int32 iflag) = {
+ *   longitude: double,
+ *   latitude: double,
+ *   distance: double,
+ *   longitudeSpeed: double,
+ *   latitudeSpeed: double,
+ *   distanceSpeed: double,
+ *   rflag: int32,
+ *   error: string
+ * }
+ */
+Handle<Value> node_swe_calc_ut (const Arguments & args) {
+	HandleScope scope;
+
+	if (args.Length () < 3) {
+		ThrowException (Exception::TypeError (String::New ("Wrong number of arguments")));
+		return scope.Close (Undefined ());
+	};
+
+	if (
+		!args [0]->IsNumber () ||
+		!args [1]->IsNumber () ||
+		!args [2]->IsNumber ()
+	) {
+		ThrowException (Exception::TypeError (String::New ("Wrong type of arguments")));
+		return scope.Close (Undefined ());
+	};
+
+	double x [6];
+	char serr [AS_MAXCH];
+	long rflag;
+
+	rflag = ::swe_calc_ut (
+		args [0]->NumberValue (),
+		(int)args [1]->NumberValue (),
+		(int)args [2]->NumberValue (),
+		x, serr
+	);
+
+	Local <Object> result = Object::New ();
+
+	if (rflag < 0) {
+		result->Set (String::NewSymbol ("error"), String::New (serr));
+	} else {
+		result->Set (String::NewSymbol ("longitude"), Number::New (x [0]));
+		result->Set (String::NewSymbol ("latitude"), Number::New (x [1]));
+		result->Set (String::NewSymbol ("distance"), Number::New (x [2]));
+		result->Set (String::NewSymbol ("longitudeSpeed"), Number::New (x [3]));
+		result->Set (String::NewSymbol ("latitudeSpeed"), Number::New (x [4]));
+		result->Set (String::NewSymbol ("distanceSpeed"), Number::New (x [5]));
+		result->Set (String::NewSymbol ("rflag"), Number::New (rflag));
+	};
+
+	return scope.Close (result);
+}
+
+
 void Initialize (Handle <Object> target) {
 	NODE_SET_METHOD (target, "swe_julday", node_swe_julday);
+	NODE_SET_METHOD (target, "swe_calc_ut", node_swe_calc_ut);
 }
 
 NODE_MODULE (swisseph, Initialize);
--- test/test.js
+++ test/test.js
@@ -1,18 +1,25 @@
 var assert = require ('assert');
+var util = require ('util');
 var swisseph = require ('..');
 
+var date;
+var julday_ut;
+var sun;
 
-{ // Julian day
-	var date = {
-		year: 2012,
-		month: 1,
-		day: 1,
-		hour: 0,
-	};
+// Test date
+date = {
+	year: 2012,
+	month: 1,
+	day: 1,
+	hour: 0,
+};
+console.log ('Test date: ' + util.inspect (date));
 
-	var julday = swisseph.swe_julday (date.year, date.month, date.day, date.hour, swisseph.SE_GREG_CAL);
+// Julian day
+julday_ut = swisseph.swe_julday (date.year, date.month, date.day, date.hour, swisseph.SE_GREG_CAL);
+assert.equal (julday_ut, 2455927.5);
+console.log ('Julian UT day for date: ', julday_ut);
 
-	assert.equal (julday, 2455927.5);
-
-	console.log ('swisseph.swe_julday (' + date.year + ', ' + date.month + ', ' + date.day + ', ' + date.hour + ', SE_GREG_CAL) =', julday);
-}
+// Sun position
+sun = swisseph.swe_calc_ut (julday_ut, 0, 256);
+console.log ('Sun position for date: ', sun);
