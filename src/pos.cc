a7353d8b4d914cea8e9d5a95d6f029860ab745fe
mivion <onopin.m.v@gmail.com>
2012-05-18 14:35:58 +0400

    Test fix

--- src/pos.cc
+++ src/pos.cc
@@ -162,16 +162,15 @@ Handle <Value> node_swe_fixstar (const Arguments & args) {
 	double x [6];
 	char serr [AS_MAXCH];
 	long rflag = 0;
+	char * starName = "Aldebaran";
 
-    *String::AsciiValue (args [0]->ToString ());
-/*
 	rflag = ::swe_fixstar (
-		*String::AsciiValue (args [0]->ToString ()),
-		(int)args [1]->NumberValue (),
+//		*String::AsciiValue (args [0]->ToString ()),
+		starName,
+		args [1]->NumberValue (),
 		(int)args [2]->NumberValue (),
 		x, serr
 	);
-*/
 
 	Local <Object> result = Object::New ();
 
