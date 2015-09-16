25862ac54ddc3bd519f954d2f39122f4a253cf74
Charandas Batra-Daitch <charandas108@gmail.com>
2015-09-15 23:03:16 -0400

    Fix the remaining issue with Nan::New
    Its actually not needed since Handles are Locals now
    See https://github.com/nodejs/nan/issues/401
    
    Local<Value> cannot be newed since Value is the base class.

--- src/callback.h
+++ src/callback.h
@@ -6,6 +6,6 @@
 
 using namespace v8;
 
-void HandleCallback (Nan::NAN_METHOD_ARGS_TYPE info, Handle <Value> result);
+void HandleCallback (Nan::NAN_METHOD_ARGS_TYPE info, Local <Value> result);
 
 #endif // __swisseph_callback_h__
