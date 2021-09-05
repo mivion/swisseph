bf27acac157587995f96e54fa815e5ef70f71d5b
Krisztian Balla <k.balla@gmx.at>
2021-09-05 21:55:03 +0200

    Add typing for functions: swe_rad_midp, swe_deg_midp, swe_split_deg, swe_csnorm, swe_difcsn, swe_difdegn, swe_difcs2n, swe_difdeg2n, swe_difrad2n, swe_csroundsec, swe_d2l, swe_day_of_week, swe_cs2timestr, swe_cs2lonlatstr and swe_cs2degstr

--- lib/swisseph.d.ts
+++ lib/swisseph.d.ts
@@ -122,12 +122,205 @@ declare namespace swisseph {
     function swe_degnorm(x: number, callback?: (result: ReturnType<typeof swe_degnorm>) => void): { x360: number };
 
     /**
-     * Normalize a radian to the range 0 - 2 PI.
+     * Normalizes a radian to the range 0 - 2 PI.
      * @param x The radian to normalize.
      * @param callback Optional callback called with the result.
      * @returns The result of the normalization.
      */
     function swe_radnorm(x: number, callback?: (result: ReturnType<typeof swe_radnorm>) => void): { x2Pi: number };
+
+    /**
+     * Return the mid point between two radians.
+     * @param x1 The first radian.
+     * @param x0 The second radian.
+     * @param callback Optional callback called with the result.
+     * @returns The result of the computation.
+     */
+    function swe_rad_midp(
+        x1: number,
+        x0: number,
+        callback?: (result: ReturnType<typeof swe_rad_midp>) => void
+    ): { xMid2Pi: number };
+
+    /**
+     * Return the mid point between two degrees.
+     * @param x1 The first degree.
+     * @param x0 The second degree.
+     * @param callback Optional callback called with the result.
+     * @returns The result of the computation.
+     */
+    function swe_deg_midp(
+        x1: number,
+        x0: number,
+        callback?: (result: ReturnType<typeof swe_rad_midp>) => void
+    ): { xMid360: number };
+
+    /**
+     * Split degrees to sign/nakshatra, degrees, minutes, seconds of arc.
+     * @param ddeg Decimal degrees, ecliptic longitude.
+     * @param roundflag By default there is no rounding. If rounding is required, the following bits can be set:
+     *                  SE_SPLIT_DEG_ROUND_SEC, SE_SPLIT_DEG_ROUND_MIN, SE_SPLIT_DEG_ROUND_DEG,
+     *                  SE_SPLIT_DEG_ZODIACAL, SE_SPLIT_DEG_KEEP_SIGN and SE_SPLIT_DEG_NAKSHATRA.
+     * @param callback Optional callback called with the result.
+     * @returns The result of the splitting.
+     *          (degree = degree integer, min = degree minutes, second = degree seconds,
+     *           secondFraction = degree fraction of seconds, sign = zodiac sign number)
+     */
+    function swe_split_deg(
+        ddeg: number,
+        roundflag: number,
+        callback?: (result: ReturnType<typeof swe_split_deg>) => void
+    ): {
+        degree: number;
+        min: number;
+        second: number;
+        secondFraction: number;
+        sign: number;
+    };
+
+    /**
+     * Normalize a centisecond value into the interval 0 - 360 DEG.
+     * @param p The centisecond value.
+     * @param callback Optional callback called with the result.
+     * @returns The result of the computation.
+     */
+    function swe_csnorm(p: number, callback?: (result: ReturnType<typeof swe_csnorm>) => void): { centisec360: number };
+
+    /**
+     * Distance in centisecs between p1 and p2 normalized to 0 - 360.
+     * @param p1 The first centisecond value
+     * @param p2 The second centisecond value
+     * @param callback Optional callback called with the result.
+     * @returns The result of the computation.
+     */
+    function swe_difcsn(
+        p1: number,
+        p2: number,
+        callback?: (result: ReturnType<typeof swe_difcsn>) => void
+    ): { centisecDiff: number };
+
+    /**
+     * Distance in degrees between p1 and p2.
+     * @param p1 The first degree value.
+     * @param p2 The second degree value.
+     * @param callback Optional callback called with the result.
+     * @returns The result of the computation.
+     */
+    function swe_difdegn(
+        p1: number,
+        p2: number,
+        callback?: (result: ReturnType<typeof swe_difdegn>) => void
+    ): { degreeDiff: number };
+
+    /**
+     * Distance in centisecs between p1 and p2 normalized to -180 - 180.
+     * @param p1 The first centisecond value
+     * @param p2 The second centisecond value
+     * @param callback Optional callback called with the result.
+     * @returns The result of the computation.
+     */
+    function swe_difcs2n(
+        p1: number,
+        p2: number,
+        callback?: (result: ReturnType<typeof swe_difcs2n>) => void
+    ): { centisecDistance180: number };
+
+    /**
+     * Distance in degrees between p1 and p2 normalized to -180 - 180.
+     * @param p1 The first degree value.
+     * @param p2 The second degree value.
+     * @param callback Optional callback called with the result.
+     * @returns The result of the computation.
+     */
+    function swe_difdeg2n(
+        p1: number,
+        p2: number,
+        callback?: (result: ReturnType<typeof swe_difdeg2n>) => void
+    ): { degreeDistance180: number };
+
+    /**
+     * Distance in radians between p1 and p2 normalized to -PI - PI.
+     * @param p1 The first radian value.
+     * @param p2 The second radian value.
+     * @param callback Optional callback called with the result.
+     * @returns The result of the computation.
+     */
+    function swe_difrad2n(
+        p1: number,
+        p2: number,
+        callback?: (result: ReturnType<typeof swe_difrad2n>) => void
+    ): { degreeDistancePi: number };
+
+    /**
+     * Round centisecond value, but at 29.5959 always down.
+     * @param x The centisecond value to round.
+     * @param callback Optional callback called with the result.
+     * @returns The result of the computation.
+     */
+    function swe_csroundsec(
+        x: number,
+        callback?: (result: ReturnType<typeof swe_csroundsec>) => void
+    ): { centisecRound: number };
+
+    /**
+     * Convert double to long with rounding, no overflow check.
+     * @param x The double value to convert.
+     * @param callback Optional callback called with the result.
+     * @returns The result of the conversion.
+     */
+    function swe_d2l(x: number, callback?: (result: ReturnType<typeof swe_d2l>) => void): { xRound: number };
+
+    /**
+     * Returns the day of week for a Julian date.
+     * @param jd The Julian date.
+     * @param callback Optional callback called with the result.
+     * @returns Monday = 0, ... Sunday = 6.
+     */
+    function swe_day_of_week(
+        jd: number,
+        callback?: (result: ReturnType<typeof swe_day_of_week>) => void
+    ): { dayOfWeek: number };
+
+    /**
+     * Converts a centisecond value to a time string.
+     * @param t The centisecond value.
+     * @param sep Separator value.
+     * @param suppressZero Specifies whether to suppress zero values. (0 = no, 1 = yes)
+     * @param callback Optional callback called with the result.
+     * @returns The result of the conversion.
+     */
+    function swe_cs2timestr(
+        t: number,
+        sep: number,
+        suppressZero: number,
+        callback?: (result: ReturnType<typeof swe_cs2timestr>) => void
+    ): { timeString: string };
+
+    /**
+     * Converts a centisecond value to a longitude or latitude string.
+     * @param t The centisecond value.
+     * @param pchar Character used for positive values.
+     * @param mchar Character used for negative values.
+     * @param callback Optional callback called with the result.
+     * @returns The result of the conversion.
+     */
+    function swe_cs2lonlatstr(
+        t: number,
+        pchar: string,
+        mchar: string,
+        callback?: (result: ReturnType<typeof swe_cs2lonlatstr>) => void
+    ): { lonlatString: string };
+
+    /**
+     * Converts a centisecond value to a degree string.
+     * @param t The centisecond value.
+     * @param callback Optional callback called with the result.
+     * @returns The result of the conversion.
+     */
+    function swe_cs2degstr(
+        t: number,
+        callback?: (result: ReturnType<typeof swe_cs2degstr>) => void
+    ): { degreeString: string };
     // #endregion util
 
     // #region date
